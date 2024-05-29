3a868469eb8592db2cecc3b86b96a87b8bc17acd
vlaszdunov <design@formo.design>
2024-05-30 00:01:35 +0300

    shutdown server

--- settings.py
+++ settings.py
@@ -5,4 +5,5 @@ import dotenv
 def config():
     dotenv.load_dotenv('.env')
     TIMEWEB_API_TOKEN = os.getenv('TIMEWEB_API_TOKEN')
+    del os.environ['TIMEWEB_API_TOKEN']
     return TIMEWEB_API_TOKEN
--- test.py
+++ test.py
@@ -1,13 +1,20 @@
 import requests
 from settings import config
-import os
 from BearierAuth import BearerAuth
 
 TIMEWEB_API_TOKEN = config()
-del os.environ['TIMEWEB_API_TOKEN']
 
 API_URL = 'https://api.timeweb.cloud/api/v1/'
 
-response = requests.get(API_URL+'projects',
-                        auth=BearerAuth(TIMEWEB_API_TOKEN))
-print(response.json())
+list_of_projects = requests.get(API_URL+'projects',
+                                auth=BearerAuth(TIMEWEB_API_TOKEN)).json()['projects']
+
+for project in list_of_projects:
+    if project['name'] == 'Test CI/CD':
+        list_of_project_servers = requests.get(API_URL+f'projects/{project['id']}/resources/servers',
+                                               auth=BearerAuth(TIMEWEB_API_TOKEN)).json()['servers']
+for server in list_of_project_servers:
+    if server['comment'] == 'Testing CI-CD':
+        a = requests.post(API_URL+f'servers/{server['id']}/action', data={'action': 'shutdown'},
+                          auth=BearerAuth(TIMEWEB_API_TOKEN))
+print(a)
